4c1760538a6a5ded2a9154807821c4faa4c79718
zhouxiang1991 <zhouxiang1991@caifujilu.com>
2019-04-02 15:48:46 +0800

    增加vue.config.js、App.vue 模版文件

--- templates/wisedu/vue.config.js
+++ templates/wisedu/vue.config.js
@@ -1,1 +1,19 @@
-module.exports = {}
+const publicPath = '/'
+const api = `${publicPath}api`
+
+module.exports = {
+  lintOnSave: true,
+  devServer: {
+    port: 8080,
+    disableHostCheck: true,
+    proxy: {
+      [api]: {
+        target: '',
+        changeOrigin: true,
+        pathRewrite: {
+          [`^${api}`]: ''
+        }
+      }
+    }
+  }
+}
