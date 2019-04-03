f144cd6e1e00b0218c32e85e839a70e335d84959
zhouxiang1991 <zhouxiang1991@caifujilu.com>
2019-04-03 09:07:41 +0800

    在vue.config.js中增加对wec-frame编译的配置

--- templates/wisedu/vue.config.js
+++ templates/wisedu/vue.config.js
@@ -3,6 +3,21 @@
 
 module.exports = {
   lintOnSave: true,
+  chainWebapck(config) {
+    config.module
+      .rule('wec-frame')
+      .test(/\.js$/)
+      .include
+      .add(/wec-frame/)
+      .end()
+      .use('babel')
+      .loader('babel-loader')
+      .options({
+        presets: [
+          ['@babel/preset-env', { modules: false }]
+        ]
+      })
+  },
   devServer: {
     port: 8080,
     disableHostCheck: true,
