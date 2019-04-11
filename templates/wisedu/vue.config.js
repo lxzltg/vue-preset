81b4a4f952758a90773ac7cecfcf24e53515e77c
zhouxiang1991 <zhouxiang1991@caifujilu.com>
2019-04-11 09:53:25 +0800

    vue.config.js增加加载svg配置

--- templates/wisedu/vue.config.js
+++ templates/wisedu/vue.config.js
@@ -4,6 +4,17 @@
 module.exports = {
   lintOnSave: true,
   chainWebpack(config) {
+    const FILE_RE = /\.(vue|js|ts|svg)$/
+    config.module.rule('svg').issuer(file => !FILE_RE.test(file))
+    config.module
+      .rule('svg-component')
+      .test(/\.svg$/)
+      .issuer(file => FILE_RE.test(file))
+      .use('vue')
+      .loader('vue-loader')
+      .end()
+      .use('svg-to-vue-component')
+      .loader('svg-to-vue-component/loader')
     config.module
       .rule('wec-frame')
       .test(/\.js$/)
